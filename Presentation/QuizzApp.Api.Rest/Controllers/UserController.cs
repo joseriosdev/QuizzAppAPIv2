f654481b7f2ec0e7d8079d6ef4aae2a983a944f6
Jose Rios <joseriosdev@gmail.com>
2023-08-21 09:12:12 -0500

    init user controller

---
+++ Presentation/QuizzApp.Api.Rest/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizzApp.Ports.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizzApp.Api.Rest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            ArgumentNullException.ThrowIfNull(userService);
+            _userService = userService;
+        }
+    }
+}
